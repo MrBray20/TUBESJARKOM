3056d947cb65cd3462eb4e7c7f883c54a4c018b3
MrBray20 <brianmone2@gmail.com>
2024-05-29 10:49:34 +0700

    menambahkan workspace jarkom

--- backendjarkom/src/main/java/com/serverjarkom/Server.java
+++ backendjarkom/src/main/java/com/serverjarkom/Server.java
@@ -5,9 +5,12 @@ import java.net.*;
 
 public class Server {
     public static final int serverPort = 6789;
-    
 
-    public static void main (String[] args){
 
+    public static void main (String[] args) throws Exception{
+        ServerSocket welcomSocket = new ServerSocket(serverPort);
+        System.out.println("TCP Server Up...");
+        while (true) {
+        }
     }
 }
