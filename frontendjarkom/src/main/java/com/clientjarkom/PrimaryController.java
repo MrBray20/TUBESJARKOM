3e8ee2ddf98b967789f6132a6cfd264b757f4313
MrBray20 <brianmone2@gmail.com>
2024-05-30 16:28:02 +0700

    mengganti versi java ke 20

--- frontendjarkom/src/main/java/com/clientjarkom/PrimaryController.java
+++ frontendjarkom/src/main/java/com/clientjarkom/PrimaryController.java
@@ -2,9 +2,13 @@ package com.clientjarkom;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
 
+    @FXML
+    private Button primaryButton;
+    
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
